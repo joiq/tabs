e1f9a7b8d0f3bd6b64eaf9e4b781bc39d6d28938
Sasha <frozeefrozee12345@gmail.com>
2020-12-11 20:46:32 +0300

    Add a timer

--- js/script.js
+++ js/script.js
@@ -37,4 +37,52 @@ window.addEventListener('DOMContentLoaded', () => {
 
   hideContent();
   showContent();
+
+  // Timer
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  
+
+  function setClock(selector) {
+    const timer = document.querySelector(selector),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timerTitles = timer.querySelectorAll('.timer__title'),
+          timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = new Date();
+
+      hours.textContent = getZero(t.getHours());
+      minutes.textContent = getZero(t.getMinutes());
+      seconds.textContent = getZero(t.getSeconds());
+      // checkNumber(t);
+    }
+
+    // function checkNumber(time) {
+    //   // if (time.getHours == 0 || time.getHours >= 5 && time.getHours <= 20) {
+    //   //   timerTitles[0].textContent = 'часов';
+    //   if (time.getHours == 20) {
+    //     timerTitles[0].textContent = 'ЧАСОВ';
+      
+    //   } else if (time.getHours >= 2 && time.getHours <= 4 &&time.getHours >= 22 && time.getHours <= 24) {
+    //     timerTitles[0].textContent = 'часа';
+    //   } else {
+    //     timerTitles[0].textContent = 'час';
+    //   }
+    //   console.log(time.getHours());
+    // }
+  }
+
+  setClock('.timer');
 });
